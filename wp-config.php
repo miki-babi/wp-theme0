ea6f57a263fa2ab21a88915fcb400cd7dcb415af
miki-babi <mikiyasshiferaw@gmail.com>
2025-02-24 02:58:26 +0300

    changed wp-config

--- wp-config.php
+++ wp-config.php
@@ -17,22 +17,22 @@
  *
  * @package WordPress
  */
-// 
+//
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'enat' );
+define( 'DB_NAME', 'axumcoff_enat' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'axumcoff_enat' );
 
 /** Database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', ')H2tO+XS^VOi' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
 
 /** Database charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf8mb4' );
+define( 'DB_CHARSET', 'utf8mb3_unicode_ci' );
 
 /** The database collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
