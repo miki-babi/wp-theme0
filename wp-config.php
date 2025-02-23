24b53e62d966b0d97f5271ca26a00e9aec4abdcd
miki-babi <mikiyasshiferaw@gmail.com>
2025-02-24 02:55:19 +0300

    changed wp-config

--- wp-config.php
+++ wp-config.php
@@ -17,22 +17,22 @@
  *
  * @package WordPress
  */
-//
+// 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'axumcoff_enat' );
+define( 'DB_NAME', 'enat' );
 
 /** Database username */
-define( 'DB_USER', 'axumcoff_enat' );
+define( 'DB_USER', 'root' );
 
 /** Database password */
-define( 'DB_PASSWORD', ')H2tO+XS^VOi' );
+define( 'DB_PASSWORD', '' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
 
 /** Database charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf8mb3_unicode_ci' );
+define( 'DB_CHARSET', 'utf8mb4' );
 
 /** The database collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
@@ -51,3 +51,53 @@ define( 'DB_COLLATE', '' );
 define( 'AUTH_KEY',         ',b.L!=Xas FA]3n^XDd*=RrIP,43I_^ca^{kFS/v5o3{VXJpZ2?<vs~wLsum~7XQ' );
 define( 'SECURE_AUTH_KEY',  'wQuC*9%}4xse3(#I?y-]sL?MvBR/q7[*]Tw6uTr _8CF QKbR)u&h[`D0PoS9,If' );
 define( 'LOGGED_IN_KEY',    'U[#^*Bom*eX4_I1=UpQQ9E:~CJ2:A&tb721d7WMz>^r0>_WQwpG3fCgsa3WgaO#T' );
+define( 'NONCE_KEY',        'BVV]|&9tFl#QqJ/RFWC@J1(Xt.*>sS(P7d+|[wVNHE)IARq&hjo]ZVyY|;-xUC08' );
+define( 'AUTH_SALT',        '6~}PMckY<0R?JxzXlx&>6&F/-eYMr3&Eu~j6(3RhzuB4^&^;{~s:4*8nbN^5cov=' );
+define( 'SECURE_AUTH_SALT', 'Bjvbz6Eo<n)hvhv2Rl%A~rU+g>~Fl:k~v<G8Rhqy5&fJk,[8]+v@6QAA[:s E+-(' );
+define( 'LOGGED_IN_SALT',   '6R;sq,%EC{fj;AM<s9qTY`u!LIsG=mDz.=]`^G^n2Y|H 0tU|a*L`w2=[_NQ^x;x' );
+define( 'NONCE_SALT',       '*A_jD|C`u[bEC;7+TH7gt[VAem`3u|?ct^Bqiu$/L9,fUC}bEcB698piv R2Dqe#' );
+
+/**#@-*/
+
+/**
+ * WordPress database table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ *
+ * At the installation time, database tables are created with the specified prefix.
+ * Changing this value after WordPress is installed will make your site think
+ * it has not been installed.
+ *
+ * @link https://developer.wordpress.org/advanced-administration/wordpress/wp-config/#table-prefix
+ */
+$table_prefix = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the documentation.
+ *
+ * @link https://developer.wordpress.org/advanced-administration/debug/debug-wordpress/
+ */
+define( 'WP_DEBUG', false );
+define( 'WP_CACHE', false );
+
+/* Add any custom values between this line and the "stop editing" line. */
+
+
+
+/* That's all, stop editing! Happy publishing. */
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', __DIR__ . '/' );
+}
+
+/** Sets up WordPress vars and included files. */
+require_once ABSPATH . 'wp-settings.php';
