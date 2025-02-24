9ed076d87f20bd303e71c4bb70ec9e008aab8856
miki-babi <mikiyasshiferaw@gmail.com>
2025-02-24 11:41:08 +0300

    changed the header to blackand config

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 //
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'enat' );
+define( 'DB_NAME', 'axumcoff_enat' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'axumcoff_enat' );
 
 /** Database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', ')H2tO+XS^VOi' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
