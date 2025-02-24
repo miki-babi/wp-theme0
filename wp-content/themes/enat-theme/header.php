43808536fafed1135aa8145f9f9fcb519b76f965
miki-babi <mikiyasshiferaw@gmail.com>
2025-02-24 11:17:49 +0300

    testing

--- wp-content/themes/enat-theme/header.php
+++ wp-content/themes/enat-theme/header.php
@@ -9,7 +9,7 @@
     <?php wp_head(); ?>
   </head>
   <body <?php body_class(); ?>>
-    <div class="bg-black text-white">
+    <div class="bg-yellow-300 text-white">
       <div class="max-w-4xl mx-auto mb-5 px-4 flex justify-between items-center">
         <h1 class="text-xl sm:text-3xl py-10"><a href="<?php echo get_home_url(); ?>" class="hover:text-teal-600">Enat bank</a></h1>
       </div>
