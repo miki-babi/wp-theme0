43d9f25beebad894e8f3a21c5eeaf3da18db66da
miki-babi <mikiyasshiferaw@gmail.com>
2025-03-06 02:56:25 +0300

    made three divs for the header

--- wp-content/themes/enat-theme/header.php
+++ wp-content/themes/enat-theme/header.php
@@ -24,6 +24,13 @@
             <a href="<?php echo get_home_url(); ?>" class="text-gray-900 hover:text-teal-600 inline-flex items-center px-1 pt-1 text-sm font-medium">Enat banko</a>
             <a href="<?php echo get_home_url(); ?>" class="text-gray-900 hover:text-teal-600 inline-flex items-center px-1 pt-1 text-sm font-medium">Enat banko</a>
           </div>
+          <div class="flex items-center">
+            <a href="#" class="text-gray-900 hover:text-teal-600 inline-flex items-center px-1 pt-1 text-sm font-medium">
+              <svg class="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-4.35-4.35m1.35-5.65a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+              </svg>
+            </a>
+          </div>
         </div>
       </div>
     </nav>
