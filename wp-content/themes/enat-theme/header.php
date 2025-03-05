87ebd07d5f41429fcd0722e524f07de392d86efd
miki-babi <mikiyasshiferaw@gmail.com>
2025-03-06 02:19:06 +0300

    added logo

--- wp-content/themes/enat-theme/header.php
+++ wp-content/themes/enat-theme/header.php
@@ -9,10 +9,12 @@
     <?php wp_head(); ?>
   </head>
   <body <?php body_class(); ?>>
-    <div class="bg-blue-500 text-white">
+    <div class="bg-white">
       <div class="max-w-4xl mx-auto mb-5 px-4 flex justify-between items-center">
-        <h1 class="text-xl sm:text-3xl py-10"><a href="<?php echo get_home_url(); ?>" class="hover:text-teal-600">Enat banko</a></h1>
-        <h1 class="text-xl sm:text-3xl py-10"><a href="<?php echo get_home_url(); ?>" class="hover:text-teal-600">Enat banko</a></h1>
-        <h1 class="text-xl sm:text-3xl py-10"><a href="<?php echo get_home_url(); ?>" class="hover:text-teal-600">Enat banko</a></h1>
+        <div class="text-xl sm:text-3xl py-10"><a href="<?php echo get_home_url(); ?>" class="hover:text-teal-600">
+          <img src="./assets/images/logo.png" alt="">
+        </a></div>
+        <div class="text-xl sm:text-3xl py-10"><a href="<?php echo get_home_url(); ?>" class="hover:text-teal-600">Enat banko</a></div>
+        <div class="text-xl sm:text-3xl py-10"><a href="<?php echo get_home_url(); ?>" class="hover:text-teal-600">Enat banko</a></div>
       </div>
     </div>
