301fd6e0defad7a4e13a8686385222291d8810a1
miki-babi <mikiyasshiferaw@gmail.com>
2025-03-07 13:32:52 +0300

    test

--- wp-content/themes/enat-theme/page-home.php
+++ wp-content/themes/enat-theme/page-home.php
@@ -14,12 +14,9 @@ get_header(); ?>
             the_post();
 
             // Include the page content template.
-            get_template_part( 'template-parts/content', 'page' );
+            get_template_part( 'template-parts/page', 'home' );
 
-            // If comments are open or we have at least one comment, load up the comment template.
-            if ( comments_open() || get_comments_number() ) :
-                comments_template();
-            endif;
+            
 
         // End the loop.
         endwhile;
@@ -28,5 +25,5 @@ get_header(); ?>
     </main><!-- .site-main -->
 </div><!-- .content-area -->
 
-<?php get_sidebar(); ?>
+
 <?php get_footer(); ?>
