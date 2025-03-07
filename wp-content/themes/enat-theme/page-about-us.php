0ec99bf1ad954d29b50873e61594f6cd8acb473d
miki-babi <mikiyasshiferaw@gmail.com>
2025-03-07 14:08:02 +0300

    test

--- wp-content/themes/enat-theme/page-about-us.php
+++ wp-content/themes/enat-theme/page-about-us.php
@@ -12,9 +12,10 @@ get_header(); ?>
         // Start the loop.
         while ( have_posts() ) :
             the_post();
+            the_content();
+
             // Include the page content template.
             get_template_part( 'template-parts/page', 'about-us' );
-            the_content();
 
 
             
