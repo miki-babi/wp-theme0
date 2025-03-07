21c7b21df6b3e0a2c525dc26ff2329d3d5813d67
miki-babi <mikiyasshiferaw@gmail.com>
2025-03-07 14:05:07 +0300

    test

---
+++ wp-content/themes/enat-theme/page-careers.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Template Name: Home Page
+ */
+
+get_header(); ?>
+
+<div id="primary" class="content-area">
+    <main id="main" class="site-main" role="main">
+
+        <?php
+        // Start the loop.
+        while ( have_posts() ) :
+            the_post();
+            // Include the page content template.
+            get_template_part( 'template-parts/page', 'careers' );
+            the_content();
+
+
+            
+
+        // End the loop.
+        endwhile;
+        ?>
+
+    </main><!-- .site-main -->
+</div><!-- .content-area -->
+
+
+<?php get_footer(); ?>
