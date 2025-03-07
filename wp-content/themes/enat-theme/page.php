9359172eac7f2b3aac3e72da3d7c28020d8a8de5
miki-babi <mikiyasshiferaw@gmail.com>
2025-03-07 13:50:16 +0300

    test

--- wp-content/themes/enat-theme/page.php
+++ wp-content/themes/enat-theme/page.php
@@ -16,13 +16,7 @@ get_header(); ?>
 <div id="primary" class="content-area">
     <main id="main" class="site-main">
 
-    <?php
-    while ( have_posts() ) :
-        the_post();
-        the_content();
-    endwhile; // End of the loop.
-    ?>
-
+    
     </main><!-- #main -->
 </div><!-- #primary -->
 
