a555a0f6022ed11741eb7b8aa1da30fd4012539a
miki-babi <mikiyasshiferaw@gmail.com>
2025-05-01 12:23:27 +0300

    elementor support

--- wp-content/themes/enat-theme/index.php
+++ wp-content/themes/enat-theme/index.php
@@ -5,13 +5,13 @@ get_header(); ?>
 <div class="max-w-4xl mx-auto px-4">
 
   <div class="prose max-w-full">
-    <h1 class="text-3xl font-bold mb-4">Welcome to Enat Bank</h1>
     <?php if (have_posts()) {
       while(have_posts()) {
         the_post(); ?>
         <div>
-          <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-          <?php echo wp_trim_words(get_the_content(), 40, '...') ?>
+          <?php 
+          // echo wp_trim_words(get_the_content(), 40, '...') 
+          ?>
           <div>
             <?php the_content(); ?>
           </div>
