b397b0e2e51223bc083bd363584bcb6b4362d17e
miki-babi <mikiyasshiferaw@gmail.com>
2025-05-06 17:08:58 +0300

    exrate

---
+++ wp-content/plugins/exrate/exrate.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+    * Plugin Name: Exrate
+    * Description: A plugin to display exchange rates.
+    * Version: 1.0  
+    * Author: Mikiyas Shiferaw
+    * Author URI: https://t.me/mikiyas_sh
+
+    * License: GPL2
+
+*/
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly.
+}
+
