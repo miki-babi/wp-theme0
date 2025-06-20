75c90abb31c0f52aaff26c93d63aa09b440abef2
miki-babi <mikiyasshiferaw@gmail.com>
2025-06-20 16:46:10 +0300

    ex test

--- wp-content/plugins/exrate/exrate.php
+++ wp-content/plugins/exrate/exrate.php
@@ -13,3 +13,25 @@
 if ( ! defined( 'ABSPATH' ) ) {
     exit; // Exit if accessed directly.
 }
+
+function exrate_shortcode() {
+    // Example exchange rates data
+    $rates = [
+        ['Currency' => 'USD', 'Rate' => '54.00'],
+        ['Currency' => 'EUR', 'Rate' => '58.00'],
+        ['Currency' => 'GBP', 'Rate' => '67.00'],
+    ];
+
+    ob_start();
+    echo '<table border="1" cellpadding="5" cellspacing="0">';
+    echo '<tr><th>Currency</th><th>Rate</th></tr>';
+    foreach ($rates as $rate) {
+        echo '<tr>';
+        echo '<td>' . esc_html($rate['Currency']) . '</td>';
+        echo '<td>' . esc_html($rate['Rate']) . '</td>';
+        echo '</tr>';
+    }
+    echo '</table>';
+    return ob_get_clean();
+}
+add_shortcode('exrate_table', 'exrate_shortcode');
