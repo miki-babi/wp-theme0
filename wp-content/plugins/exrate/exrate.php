958bc27b34873041c8d53464a17b1b453c095403
miki-babi <mikiyasshiferaw@gmail.com>
2025-06-25 14:34:55 +0300

    tete

--- wp-content/plugins/exrate/exrate.php
+++ wp-content/plugins/exrate/exrate.php
@@ -209,7 +209,7 @@ add_shortcode('currency_table', function () {
             <h2 x-text="selected.name"></h2>
             <span class="rate" x-text="selected.rate"></span>
           </div>
-          <div>
+          <div class="exchange-info">
             <ul class="rate-list">
               <li class= "currency-list "><span >Cash Buying</span><span x-text="selected.cash_buy" class="currency-span"></span></li>
               <li class= "currency-list "><span>Cash Selling</span><span x-text="selected.cash_sell" class="currency-span"></span></li>
