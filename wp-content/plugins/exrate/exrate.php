f608a0c8538b5d94e1ae2937023ef8008a9fce74
miki-babi <mikiyasshiferaw@gmail.com>
2025-06-25 15:42:34 +0300

    tete

--- wp-content/plugins/exrate/exrate.php
+++ wp-content/plugins/exrate/exrate.php
@@ -256,7 +256,7 @@ add_shortcode('currency_table', function () {
         max-width: 100%;
         margin: 0 auto;
         padding: 20px;
-        border: 2px solid red;
+        /* border: 2px solid red; */
         border-radius: 12px;
         box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
       }
