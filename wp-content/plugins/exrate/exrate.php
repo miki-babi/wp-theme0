6d92ec88fa23a9910dd76a2f7cdfe6d9c6528f0b
miki-babi <mikiyasshiferaw@gmail.com>
2025-06-25 15:45:33 +0300

    update

--- wp-content/plugins/exrate/exrate.php
+++ wp-content/plugins/exrate/exrate.php
@@ -257,13 +257,14 @@ add_shortcode('currency_table', function () {
         margin: 0 auto;
         padding: 20px;
         /* border: 2px solid red; */
-        border-radius: 12px;
+
+        border-radius: 40px;
         box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
       }
       .currency-widget {
         /* border: 2px solid #0070c0; */
         
-        padding: 20px;
+        padding: 40px;
         font-family: Arial, sans-serif;
         background: #EAECF2;
       }
