6e8006102ddb315c798d6372243395d1fc4595f2
miki-babi <mikiyasshiferaw@gmail.com>
2025-06-29 19:22:09 +0300

    red

--- wp-content/plugins/location/atm-template.php
+++ wp-content/plugins/location/atm-template.php
@@ -87,7 +87,7 @@ $first_iframe = "<iframe src=\"https://maps.google.com/maps?q={$first_lat},{$fir
 
     /* Red highlight for the last clicked item */
     .atm-item.active {
-        background-color: #ffcccc;
+        background-color:rgb(245, 0, 0);
         border-color: #ff0000;
         color: #900000;
         font-weight: bold;
