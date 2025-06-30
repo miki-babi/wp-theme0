c5d3a29f800d627505df77d5f1cd40481f21242c
miki-babi <mikiyasshiferaw@gmail.com>
2025-06-30 08:07:57 +0300

    ui

--- wp-content/plugins/location/atm-template.php
+++ wp-content/plugins/location/atm-template.php
@@ -82,14 +82,14 @@ body {
 }
 
 .atm-item:hover {
-    background-color: rgba(0, 255, 13, 0.45);
+    border-color:rgba(255, 0, 0, 0.57);
     border-color: #0073e6;
     transform: translateY(-2px);
 }
 
 .atm-item.active {
     background-color: #ffcccc;
-    border-color: #ff0000;
+    border-color:rgba(255, 0, 0, 0.57);
     color: #900000;
     font-weight: bold;
 }
